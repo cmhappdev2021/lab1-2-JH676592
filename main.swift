69711c33fce6644b5240b7ed5427f431c22822bf
JH676592 <jacksonholcomb01@gmail.com>
2020-09-02 18:42:50 +0000

    Created a constant

--- main.swift
+++ main.swift
@@ -1,25 +1,25 @@
 // Declare a constant called friends to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
 let friends = 75
-print(friends)
+print(friends)clear
 
 // Now assume you go through and remove friends that aren't active on social media. Attempt to update your friends constant to a lower number than it currently is. Observe what happens and then move to the next step.
-friends = friends - 5
 print(friends) // Cannot change the value of a constant.
 
 // Does the above code compile? Why not? Print your explanation to the console using the print function. Go back and delete your line of code that updates the friends constant to a lower number so that the playground will compile properly.
-print("No, the code does not compile correctly because you cannot change the value of a constant.") 
+print("No, the code does not compile correctly because you cannot change the value of a constant.")
 
 // Note: These exercises reinforce Swift concepts in the context of a fitness tracking app.
 // Your fitness tracking app needs to know goal number of steps per day. Create a constant goalSteps and set it to 10000.
 let goalSteps: Int
 goalSteps = 10000
 
-
 // Use two print functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of goalSteps by referencing your constant.
-
+print("Your step goal for the day is:")
+print(goalSteps)
 
 // Declare a variable schooling and set it to the number of years of school that you have completed. Print schooling to the console.
-
+var schooling = 11
+print(schooling)
 
 // Now imagine you just completed an additional year of school, and update the schooling variable accordingly. Print schooling to the console.
 
